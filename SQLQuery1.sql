1808e9a6e0c475f68950e37389506aeab9547d3a
FELIPE CEZAR DA SILVA DOS SANTOS <felipe.santos@nutriave.localnet>
2021-12-06 12:13:43 -0300

    curso SQL_2

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -23,19 +23,19 @@ FROM Person.Person;
 
 SELECT FROM coluna1,coluna2,colunan
 FROM tabela
-WHERE condiÁ„o;
+WHERE condi√ß√£o;
 
 /*
 
-OPERADOR             -         DESCRI«√O
+OPERADOR             -         DESCRI√á√ÉO
 =								IGUAL
 >								MAIOR QUE
 <								MENOR QUE
 >=								MAIOR QUE OU IGUAL
 <=								MENOR QUE OU IGUAL
 <>								DIFERENTE DE
-AND								OPERADOR L”GICO E
-OR								OPERADOR L”GICO OU
+AND								OPERADOR L√ìGICO E
+OR								OPERADOR L√ìGICO OU
 
 */
 
@@ -61,24 +61,24 @@ WHERE color <> 'red'
 
 -- DESAFIO 1
 
-/* a equipe de produÁ„o produtos precisa do nome
-de todas peÁas que pesam mais de 500kg mas n„o 
-mais 700kg para inspeÁ„o
+/* a equipe de produ√ß√£o produtos precisa do nome
+de todas pe√ßas que pesam mais de 500kg mas n√£o 
+mais 700kg para inspe√ß√£o
 
 -- DESAFIO 2
 
-Foi pedido pelo Marketinguma relaÁ„o de todos
-os empregados(employees) que s„o casados(single=
-solteiro, married=casado) e s„o 
+Foi pedido pelo Marketinguma rela√ß√£o de todos
+os empregados(employees) que s√£o casados(single=
+solteiro, married=casado) e s√£o 
 assalariados(salaried)
 
 -- DESAFIO 3
 
-Um usu·rio chamado de Peter Krebs  est· devendo
+Um usu√°rio chamado de Peter Krebs  est√° devendo
 um pagamento, consiga o email dele para que possamos
-enviar uma cobranÁa!
+enviar uma cobran√ßa!
 
-( vocÍ vai ter que usar a tabela person.person e depois a tabela person.emailaddress)
+( voc√™ vai ter que usar a tabela person.person e depois a tabela person.emailaddress)
 
 */
 
@@ -91,6 +91,10 @@ SELECT *
 FROM HumanResources.Employee
 WHERE MaritalStatus = 'M' AND SalariedFlag = 1;
 
-SELECT EmailAddress
-FROM Person.Person, Person.EmailAddress
+SELECT *
+FROM Person.Person
 WHERE FirstName = 'Peter' AND LastName = 'Krebs';
+
+SELECT EmailAddress
+FROM Person.EmailAddress
+WHERE BusinessEntityID = 26;
